d9355f0b2c929f3c9438e20485ef0c8539d1159a
bukale <bukale2022@163.com>
2023-03-11 15:55:32 +0800

    修复可能存在的找不到IP属地时报错的bug

--- lib/geoip.lua
+++ lib/geoip.lua
@@ -14,41 +14,42 @@ function _M.lookup(ip)
         allowCountry = (next(allowCountryList) ~= nil)
     end
 
+    local isAllowed = true
+    local result = '-'
+
     --support ipv6 e.g. 2001:4860:0:1001::3004:ef68
     local res,err = geo.lookup(ip)
 
     if not res then
         ngx.log(ngx.ERR, 'failed to lookup by ip ,reason:', err)
-    end
+    else
+        result = res['country']['names'][language]
 
-    local result = res['country']['names'][language]
-
-    local subdivisions = res['subdivisions']
-    if subdivisions then
-        local name = subdivisions[1]['names'][language]
-        if name ~= nil then
-            result = result .. name
+        local subdivisions = res['subdivisions']
+        if subdivisions then
+            local name = subdivisions[1]['names'][language]
+            if name ~= nil then
+                result = result .. name
+            end
         end
-    end
 
-    local city = res['city']
-    if city then
-        local name = city['names'][language]
-        if name ~= nil then
-            result = result .. name
+        local city = res['city']
+        if city then
+            local name = city['names'][language]
+            if name ~= nil then
+                result = result .. name
+            end
         end
-    end
 
-    local isAllowed = true
+        if allowCountry then
+            local iso_code = res['country']['iso_code']
+            isAllowed = false
 
-    if allowCountry then
-        local iso_code = res['country']['iso_code']
-        isAllowed = false
-
-        for _, c in ipairs(allowCountryList) do
-            if iso_code == c then
-                isAllowed = true
-                break
+            for _, c in ipairs(allowCountryList) do
+                if iso_code == c then
+                    isAllowed = true
+                    break
+                end
             end
         end
     end
