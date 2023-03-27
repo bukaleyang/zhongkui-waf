ecd4a72e79ec1a2c955eb9c48e0cfaa9f08184c4
bukale <bukale2022@163.com>
2023-03-27 18:01:24 +0800

    敏感数据过滤限定响应媒体格式

--- body_filter.lua
+++ body_filter.lua
@@ -1,14 +1,24 @@
 local sensitive = require "sensitive"
 local config = require "config"
 
+local ngxfind = ngx.re.find
+local CONTENT_TYPE_REGEX = "^(?:text/html|text/plain|text/xml|application/json|application/xml|application/xhtml\\+xml)"
+
 local content = ngx.arg[1]
 
 if config.isWAFOn and config.isProtectionMode then
     if ngx.status ~= 403 then
         if config.isSensitiveDataFilteringOn then
-            if content then
-                content = sensitive.sensitive_data_filtering(content)
+            local contentType = ngx.header.content_type
+            if contentType then
+                local from = ngxfind(contentType, CONTENT_TYPE_REGEX, "isjo")
+                if from then
+                    if content then
+                        content = sensitive.sensitive_data_filtering(content)
+                    end
+                end
             end
+
         end
     end
 end
