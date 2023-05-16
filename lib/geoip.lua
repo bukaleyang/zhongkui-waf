2709d766bd5d4801d3701bce1c0c9fdb75e53f72
bukale <bukale2022@163.com>
2023-05-16 15:55:27 +0800

    增加ip地理位置查找代码健壮性

--- lib/geoip.lua
+++ lib/geoip.lua
@@ -3,6 +3,8 @@ local config = require "config"
 
 local _M = {}
 
+local pcall = pcall
+
 local dbFile = config.get('geoip_db_file')
 local allowCountryList = config.get('geoip_allow_country') or {}
 local language = config.get("geoip_language") ~= '' and config.get("geoip_language") or 'en'
@@ -20,31 +22,47 @@ function _M.lookup(ip)
     local city = nil
 
     --support ipv6 e.g. 2001:4860:0:1001::3004:ef68
-    local res, err = geo.lookup(ip)
-
-    if not res then
-        ngx.log(ngx.ERR, 'failed to lookup by ip ,reason:', err)
+    local pass, res, err = pcall(geo.lookup, ip)
+    if not pass then
+        ngx.log(ngx.ERR, 'failed to lookup by ip,reason:', res)
     else
-        country = res['country']['names'][language] or ''
+        if not res then
+            ngx.log(ngx.ERR, 'failed to lookup by ip,reason:', err)
+        else
+            local countryRes = res['country']
+            if countryRes then
+                local names = countryRes['names']
+                if names then
+                    country = names[language] or ''
+                end
+            end
 
-        local subdivisions = res['subdivisions']
-        if subdivisions then
-            province = subdivisions[1]['names'][language] or ''
-        end
+            local subdivisions = res['subdivisions']
+            if subdivisions then
+                local subdivisions1 = subdivisions[1]
+                if subdivisions1 then
+                    local names = subdivisions1['names']
+                    if names then
+                        province = names[language] or ''
+                    end
+                end
+            end
 
-        local cityRes = res['city']
-        if cityRes then
-            city = cityRes['names'][language] or ''
-        end
+            local cityRes = res['city']
+            if cityRes then
+                local names = cityRes['names']
+                city = names[language] or ''
+            end
 
-        if allowCountry then
-            local iso_code = res['country']['iso_code']
-            isAllowed = false
+            if allowCountry then
+                local iso_code = res['country']['iso_code']
+                isAllowed = false
 
-            for _, c in ipairs(allowCountryList) do
-                if iso_code == c then
-                    isAllowed = true
-                    break
+                for _, c in ipairs(allowCountryList) do
+                    if iso_code == c then
+                        isAllowed = true
+                        break
+                    end
                 end
             end
         end
